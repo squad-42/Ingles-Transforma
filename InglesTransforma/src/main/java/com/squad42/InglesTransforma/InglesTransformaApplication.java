a113c1be4471ebdeca0ff50d8e0bdd5fcda5d8cd
David <david.henri.soouza@gmail.com>
2023-11-11 12:59:40 -0300

    Create:projetct

---
+++ InglesTransforma/src/main/java/com/squad42/InglesTransforma/InglesTransformaApplication.java
@@ -0,0 +1,13 @@
+package com.squad42.InglesTransforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InglesTransformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InglesTransformaApplication.class, args);
+	}
+
+}
---
+++ InglesTransforma/src/test/java/com/squad42/InglesTransforma/InglesTransformaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad42.InglesTransforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InglesTransformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
