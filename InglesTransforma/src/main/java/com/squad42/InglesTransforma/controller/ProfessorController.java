a07aff3e229d8852d174b687845e1b62cc971ee0
RabbitDeWitt <davidsocoelho@outlook.com>
2023-11-19 17:51:12 -0300

    feat:criação de cursos concluida

--- InglesTransforma/src/main/java/com/squad42/InglesTransforma/controller/ProfessorController.java
+++ InglesTransforma/src/main/java/com/squad42/InglesTransforma/controller/ProfessorController.java
@@ -1,20 +1,41 @@
 package com.squad42.InglesTransforma.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.squad42.InglesTransforma.model.Curso;
+import com.squad42.InglesTransforma.repository.CursosRepository;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/professor")
 public class ProfessorController {
+	@Autowired
+	private CursosRepository cr;
 	
 	@GetMapping
 	public ModelAndView mostrar(HttpSession session) {
+		List<Curso> cursos = cr.findAll(); 
 		ModelAndView page = new ModelAndView("/pages/professor/professor");
 		page.addObject("usuario", session.getAttribute("usuario"));
+		page.addObject("novoCurso", new Curso());
+		page.addObject("cursos", cursos);
+		
 		return page;
+	}
+	
+	
+	@PostMapping
+	public ModelAndView cadastrarCurso(Curso novoCurso) {
+	ModelAndView page = new ModelAndView("redirect:/professor");
+	cr.save(novoCurso);
+	return page;
 	}
 }
---
+++ InglesTransforma/src/main/java/com/squad42/InglesTransforma/repository/CursosRepository.java
@@ -0,0 +1,9 @@
+package com.squad42.InglesTransforma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.squad42.InglesTransforma.model.Curso;
+
+public interface CursosRepository extends JpaRepository<Curso, Integer> {
+
+}
