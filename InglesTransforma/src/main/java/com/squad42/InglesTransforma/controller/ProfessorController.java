b5dda5b5f4164cf869d4b2d3fbfeb70a039d52dd
RabbitDeWitt <davidsocoelho@outlook.com>
2023-11-19 18:38:41 -0300

    feat: Editar e Excluir cursos finalizado

--- InglesTransforma/src/main/java/com/squad42/InglesTransforma/controller/ProfessorController.java
+++ InglesTransforma/src/main/java/com/squad42/InglesTransforma/controller/ProfessorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,11 +32,24 @@ public class ProfessorController {
 		return page;
 	}
 	
-	
 	@PostMapping
 	public ModelAndView cadastrarCurso(Curso novoCurso) {
 	ModelAndView page = new ModelAndView("redirect:/professor");
 	cr.save(novoCurso);
 	return page;
+	}
+	
+	@GetMapping("/{id}/excluir")
+	public ModelAndView excluir(@PathVariable Integer id) {
+		ModelAndView page = new ModelAndView("redirect:/professor");
+		cr.deleteById(id);
+		return page;
+	}
+	
+	@PostMapping("/{id}/editar")
+	public ModelAndView editar(Curso curso) {
+		ModelAndView page = new ModelAndView("redirect:/professor");
+		cr.save(curso);
+		return page;
 	}
 }
