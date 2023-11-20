dd97e6807450d9ccb8dac45d5c6c6fa8ceac22ba
RabbitDeWitt <davidsocoelho@outlook.com>
2023-11-20 14:31:22 -0300

    fix: controller login

--- InglesTransforma/src/main/resources/static/js/professor.js
+++ InglesTransforma/src/main/resources/static/js/professor.js
@@ -14,28 +14,4 @@ footer.innerHTML = footerComponent(footer)
 
 const { cod, prof, profPic, course, thumb, numOfVid } = courses.find((couse) => couse.cod === codCurso)
 
-profile.innerHTML = `
-      <div class="tutor">
-        <img src="../../assets/imgs/pic-${profPic}.jpg" alt="">
-        <h3>${prof}</h3>
-        <span>Professor</span>
-      </div>
-      <div class="flex">
-        <p>Total de playlist: <span>1</span></p>
-        <p>Total de vídeos: <span>${numOfVid}</span></p>
-      </div>
-`
-
-coursesSection.innerHTML = `
-        <div class="box">
-          <div class="thumb">
-            <img src="../../assets/imgs/thumb-${thumb}.png" alt="">
-            <span>${numOfVid} videos</span>
-          </div>
-          <h3 class="title">${course}</h3>
-          <a href="../aulas/aulas.html" class="btn btn-dark-blue" onclick="setCourse(${cod})">ver playslist</a>
-        </div>
-`
-
-
 
